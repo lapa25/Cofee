0c721e71eae763a541ca186085f2c14339fafe73
alapa <lapantonina2505@mail.ru>
2021-11-29 21:05:39 +0300

    все готово

--- main.py
+++ main.py
@@ -2,121 +2,124 @@ import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QLabel
 import sqlite3
-from PyQt5 import uic
+from ui_file import Ui_MainWindow
+from ui_file_1 import Ui_MainWindow_1
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('main.ui', self)
+        super(MyWidget, self).__init__()
+        self.ui = Ui_MainWindow_1()
+        self.ui_1 = Ui_MainWindow()
+        self.ui.setupUi(self)
         self.conn = sqlite3.connect('coffee.sqlite')
-        self.pushButton.clicked.connect(self.run)
-        self.pushButton_2.clicked.connect(self.change)
+        self.ui.pushButton.clicked.connect(self.run)
+        self.ui.pushButton_2.clicked.connect(self.change)
 
     def change(self):
-        uic.loadUi('addEditCoffeeForm.ui', self)
-        self.radioButton.clicked.connect(self.adding)
-        self.radioButton_2.clicked.connect(self.adding)
-        self.text = QLabel()
+        self.ui_1.setupUi(self)
+        self.ui_1.radioButton.clicked.connect(self.adding)
+        self.ui_1.radioButton_2.clicked.connect(self.adding)
+        self.ui_1.text = QLabel()
         self.whatwedo = str()
-        self.statusbar.addWidget(self.text)
-        self.label.hide()
-        self.pushButton_4.hide()
-        self.plainTextEdit_4.hide()
-        self.comboBox.addItems(['молотый', 'зерновой'])
-        self.comboBox_2.addItems(['итальянская', 'сильная', 'средняя', 'слабая'])
-        self.pushButton_3.clicked.connect(self.add)
+        self.ui_1.statusbar.addWidget(self.ui_1.text)
+        self.ui_1.label.hide()
+        self.ui_1.pushButton_4.hide()
+        self.ui_1.plainTextEdit_4.hide()
+        self.ui_1.comboBox.addItems(['молотый', 'зерновой'])
+        self.ui_1.comboBox_2.addItems(['итальянская', 'сильная', 'средняя', 'слабая'])
+        self.ui_1.pushButton_3.clicked.connect(self.add)
 
     def adding(self):
-        if self.radioButton.isChecked():
+        if self.ui_1.radioButton.isChecked():
             self.whatwedo = 'add'
-            self.label_2.show()
-            self.label_3.show()
-            self.label_4.show()
-            self.label_5.show()
-            self.label_6.show()
-            self.label_7.show()
-            self.plainTextEdit_3.show()
-            self.comboBox_2.show()
-            self.plainTextEdit.show()
-            self.plainTextEdit_5.show()
-            self.plainTextEdit_6.show()
-            self.label.hide()
-            self.plainTextEdit_4.hide()
-            self.pushButton_4.hide()
-            self.comboBox.show()
-        elif self.radioButton_2.isChecked():
+            self.ui_1.label_2.show()
+            self.ui_1.label_3.show()
+            self.ui_1.label_4.show()
+            self.ui_1.label_5.show()
+            self.ui_1.label_6.show()
+            self.ui_1.label_7.show()
+            self.ui_1.plainTextEdit_3.show()
+            self.ui_1.comboBox_2.show()
+            self.ui_1.plainTextEdit.show()
+            self.ui_1.plainTextEdit_5.show()
+            self.ui_1.plainTextEdit_6.show()
+            self.ui_1.label.hide()
+            self.ui_1.plainTextEdit_4.hide()
+            self.ui_1.pushButton_4.hide()
+            self.ui_1.comboBox.show()
+        elif self.ui_1.radioButton_2.isChecked():
             self.whatwedo = 'change'
-            self.label.show()
-            self.plainTextEdit_4.show()
-            self.label_2.hide()
-            self.label_3.hide()
-            self.label_4.hide()
-            self.label_5.hide()
-            self.label_6.hide()
-            self.label_7.hide()
-            self.plainTextEdit_3.hide()
-            self.comboBox_2.hide()
-            self.plainTextEdit.hide()
-            self.plainTextEdit_5.hide()
-            self.plainTextEdit_6.hide()
-            self.comboBox.hide()
-            self.pushButton_4.show()
-            self.pushButton_4.clicked.connect(self.searching)
+            self.ui_1.label.show()
+            self.ui_1.plainTextEdit_4.show()
+            self.ui_1.label_2.hide()
+            self.ui_1.label_3.hide()
+            self.ui_1.label_4.hide()
+            self.ui_1.label_5.hide()
+            self.ui_1.label_6.hide()
+            self.ui_1.label_7.hide()
+            self.ui_1.plainTextEdit_3.hide()
+            self.ui_1.comboBox_2.hide()
+            self.ui_1.plainTextEdit.hide()
+            self.ui_1.plainTextEdit_5.hide()
+            self.ui_1.plainTextEdit_6.hide()
+            self.ui_1.comboBox.hide()
+            self.ui_1.pushButton_4.show()
+            self.ui_1.pushButton_4.clicked.connect(self.searching)
         else:
             self.whatwedo = 'aa'
 
     def searching(self):
         cur = self.conn.cursor()
-        request = 'SELECT title from name WHERE id=' + self.plainTextEdit_4.toPlainText()
+        request = 'SELECT title from name WHERE id=' + self.ui_1.plainTextEdit_4.toPlainText()
         res = cur.execute(request).fetchall()
         if len(res) != 0:
-            self.text.setText('')
-            self.plainTextEdit_4.show()
-            self.label_2.show()
-            self.label_3.show()
-            self.label_4.show()
-            self.label_5.show()
-            self.label_6.show()
-            self.label_7.show()
-            self.plainTextEdit_3.show()
-            self.comboBox_2.show()
-            self.plainTextEdit.show()
-            self.plainTextEdit_5.show()
-            self.plainTextEdit_6.show()
-            self.comboBox.show()
-            self.plainTextEdit_3.setPlainText(str(res[0][0]))
-            request = 'SELECT roasting from roasting WHERE id=' + self.plainTextEdit_4.toPlainText()
+            self.ui_1.text.setText('')
+            self.ui_1.plainTextEdit_4.show()
+            self.ui_1.label_2.show()
+            self.ui_1.label_3.show()
+            self.ui_1.label_4.show()
+            self.ui_1.label_5.show()
+            self.ui_1.label_6.show()
+            self.ui_1.label_7.show()
+            self.ui_1.plainTextEdit_3.show()
+            self.ui_1.comboBox_2.show()
+            self.ui_1.plainTextEdit.show()
+            self.ui_1.plainTextEdit_5.show()
+            self.ui_1.plainTextEdit_6.show()
+            self.ui_1.comboBox.show()
+            self.ui_1.plainTextEdit_3.setPlainText(str(res[0][0]))
+            request = 'SELECT roasting from roasting WHERE id=' + self.ui_1.plainTextEdit_4.toPlainText()
             res = cur.execute(request).fetchall()
-            self.comboBox_2.setCurrentText(str(res[0][0]))
-            request = 'SELECT type from type WHERE id=' + self.plainTextEdit_4.toPlainText()
+            self.ui_1.comboBox_2.setCurrentText(str(res[0][0]))
+            request = 'SELECT type from type WHERE id=' + self.ui_1.plainTextEdit_4.toPlainText()
             res = cur.execute(request).fetchall()
-            self.comboBox.setCurrentText(str(res[0][0]))
-            request = 'SELECT about from taste WHERE id=' + self.plainTextEdit_4.toPlainText()
+            self.ui_1.comboBox.setCurrentText(str(res[0][0]))
+            request = 'SELECT about from taste WHERE id=' + self.ui_1.plainTextEdit_4.toPlainText()
             res = cur.execute(request).fetchall()
-            self.plainTextEdit.setPlainText(str(res[0][0]))
-            request = 'SELECT volume from volume WHERE id=' + self.plainTextEdit_4.toPlainText()
+            self.ui_1.plainTextEdit.setPlainText(str(res[0][0]))
+            request = 'SELECT volume from volume WHERE id=' + self.ui_1.plainTextEdit_4.toPlainText()
             res = cur.execute(request).fetchall()
-            self.plainTextEdit_5.setPlainText(str(res[0][0]))
-            request = 'SELECT price from price WHERE id=' + self.plainTextEdit_4.toPlainText()
+            self.ui_1.plainTextEdit_5.setPlainText(str(res[0][0]))
+            request = 'SELECT price from price WHERE id=' + self.ui_1.plainTextEdit_4.toPlainText()
             res = cur.execute(request).fetchall()
-            self.plainTextEdit_6.setPlainText(str(res[0][0]))
+            self.ui_1.plainTextEdit_6.setPlainText(str(res[0][0]))
         else:
-            self.text.setText("Такого id нет")
+            self.ui_1.text.setText("Такого id нет")
 
     def add(self):
-        name = self.plainTextEdit_3.toPlainText()
-        roasting = self.comboBox_2.currentText()
-        taste = self.plainTextEdit.toPlainText()
-        volume = self.plainTextEdit_5.toPlainText()
-        price = self.plainTextEdit_6.toPlainText()
-        type = self.comboBox.currentText()
+        name = self.ui_1.plainTextEdit_3.toPlainText()
+        roasting = self.ui_1.comboBox_2.currentText()
+        taste = self.ui_1.plainTextEdit.toPlainText()
+        volume = self.ui_1.plainTextEdit_5.toPlainText()
+        price = self.ui_1.plainTextEdit_6.toPlainText()
+        type = self.ui_1.comboBox.currentText()
         if self.whatwedo == 'add':
             if len(name) == 0 or len(roasting) == 0 or len(taste) == 0 or len(volume) == 0 or \
                     len(price) == 0 or len(type) == 0:
-                self.text.setText('Заполнены не все данные')
+                self.ui_1.text.setText('Заполнены не все данные')
             elif not volume.isdigit() or not price.isdigit():
-                self.text.setText('Объем и цена - численные значения')
+                self.ui_1.text.setText('Объем и цена - численные значения')
             else:
                 cur = self.conn.cursor()
                 request = "INSERT INTO name(title) VALUES('" + name + "')"
@@ -132,38 +135,39 @@ class MyWidget(QMainWindow):
                 request = "INSERT INTO type(type) VALUES('" + type + "')"
                 cur.execute(request)
                 self.conn.commit()
-                uic.loadUi('main.ui', self)
+                self.ui.setupUi(self)
                 self.conn = sqlite3.connect('coffee.sqlite')
-                self.pushButton.clicked.connect(self.run)
-                self.pushButton_2.clicked.connect(self.change)
+                self.ui.pushButton.clicked.connect(self.run)
+                self.ui.pushButton_2.clicked.connect(self.change)
         elif self.whatwedo == 'change':
             cur = self.conn.cursor()
-            request = "UPDATE name SET title ='" + name + "' WHERE id=" + self.plainTextEdit_4.toPlainText()
+            request = "UPDATE name SET title ='" + name + "' WHERE id=" + self.ui_1.plainTextEdit_4.toPlainText()
             cur.execute(request)
-            request = "UPDATE roasting SET roasting ='" + roasting + "' WHERE id=" + self.plainTextEdit_4.toPlainText()
+            request = "UPDATE roasting SET roasting ='" + \
+                      roasting + "' WHERE id=" + self.ui_1.plainTextEdit_4.toPlainText()
             cur.execute(request)
-            request = "UPDATE taste SET about ='" + taste + "' WHERE id=" + self.plainTextEdit_4.toPlainText()
+            request = "UPDATE taste SET about ='" + taste + "' WHERE id=" + self.ui_1.plainTextEdit_4.toPlainText()
             cur.execute(request)
-            request = "UPDATE volume SET volume ='" + volume + "' WHERE id=" + self.plainTextEdit_4.toPlainText()
+            request = "UPDATE volume SET volume ='" + volume + "' WHERE id=" + self.ui_1.plainTextEdit_4.toPlainText()
             cur.execute(request)
-            request = "UPDATE price SET price ='" + price + "' WHERE id=" + self.plainTextEdit_4.toPlainText()
+            request = "UPDATE price SET price ='" + price + "' WHERE id=" + self.ui_1.plainTextEdit_4.toPlainText()
             cur.execute(request)
-            request = "UPDATE type SET type ='" + type + "' WHERE id=" + self.plainTextEdit_4.toPlainText()
+            request = "UPDATE type SET type ='" + type + "' WHERE id=" + self.ui_1.plainTextEdit_4.toPlainText()
             cur.execute(request)
             self.conn.commit()
-            uic.loadUi('main.ui', self)
+            self.ui.setupUi(self)
             self.conn = sqlite3.connect('coffee.sqlite')
-            self.pushButton.clicked.connect(self.run)
-            self.pushButton_2.clicked.connect(self.change)
+            self.ui.pushButton.clicked.connect(self.run)
+            self.ui.pushButton_2.clicked.connect(self.change)
         else:
-            self.text.setText('Ничего не выбрано')
+            self.ui_1.text.setText('Ничего не выбрано')
 
     def run(self):
-        self.tableWidget.setColumnCount(7)
-        self.tableWidget.setHorizontalHeaderLabels(
+        self.ui.tableWidget.setColumnCount(7)
+        self.ui.tableWidget.setHorizontalHeaderLabels(
             ['ID', 'название сорта', 'степень обжарки',
              'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки'])
-        self.tableWidget.setRowCount(0)
+        self.ui.tableWidget.setRowCount(0)
         request = "SELECT name.id,\n"
         request += "name.title, \n"
         request += "roasting.roasting, \n"
@@ -181,12 +185,12 @@ class MyWidget(QMainWindow):
         cur = self.conn.cursor()
         res = cur.execute(request).fetchall()
         for i in range(len(res)):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
+            self.ui.tableWidget.setRowCount(
+                self.ui.tableWidget.rowCount() + 1)
             for j in range(7):
-                self.tableWidget.setItem(
+                self.ui.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(res[i][j])))
-        self.tableWidget.resizeColumnsToContents()
+        self.ui.tableWidget.resizeColumnsToContents()
 
 
 def except_hook(cls, exception, traceback):
